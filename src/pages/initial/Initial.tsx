93f1aa2ca23da952510cffeec88e5a91886cfff5
Ipedrp <pedropaulo727mqs@gmail.com>
2025-09-27 17:09:24 -0300

    notification popup

--- src/pages/initial/Initial.tsx
+++ src/pages/initial/Initial.tsx
@@ -1,9 +1,15 @@
 import { Cards } from "@/components/card/Cards";
 import { ChartColumnStacked, ScanBarcode, Images, Waypoints } from "lucide-react";
 import { Link } from "react-router";
-import { Notification } from "@/components/notification/Notification";
-// Home.tsx
+import { useState } from "react"
+import { Sucess } from "@/components/notification/Sucess";
+import { Error } from "@/components/notification/Error";
+
 export function Initial() {
+
+  const [showNotification, setShowNotification] = useState(false)
+  const [showNotificationError, setShowNotificationError] = useState(false)
+
   return (
     <div>
       <section className="grid grid-cols-1 sm:grid-cols-1 md:grid-cols-4 lg:grid-cols-4 gap-4 p-4 w-full">
@@ -20,9 +26,26 @@ export function Initial() {
           <Cards title="Imagens" icon={<Images />} />
         </Link>
       </section>
+
+      {/* ativar o button para parecer a notificação */}
       <div>
-        <Notification title="teste" description="testando" close="fechar"/>
+        <button onClick={() => setShowNotification(true)}>Mostrar Sucess notificação</button>
+        <Sucess
+          active={showNotification}
+          onClose={() => setShowNotification(false)}
+          text={"Sucess"}
+        />
+      </div>
+
+      {/* ativar o button para parecer a notificação */}
+      <div>
+        <button onClick={() => setShowNotificationError(true)}>Mostrar Error notificação</button>
+        <Error
+          active={showNotificationError}
+          onClose={() => setShowNotificationError(false)}
+          text={"Error"}
+        />
       </div>
     </div>
-  );
+  )
 }
